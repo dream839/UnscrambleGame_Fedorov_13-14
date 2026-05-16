ae03ed1d6e3d1cb1abb2eac27749ec0815835c22
dream839 <mihailfedorov3973@gmail.com>
2026-05-16 11:04:53 +0300

    Step 3: Create base GameViewModel class

---
+++ app/src/main/java/com/fedorov/unscramblegame_13_14/ui_model/GameViewModel.kt
@@ -0,0 +1,19 @@
+package com.fedorov.unscramblegame_13_14.ui_model
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.fedorov.unscramblegame_13_14.data.GameUiState
+
+class GameViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    init {
+        resetGame()
+    }
+
+    fun resetGame() {}
+    // TODO:
+}
